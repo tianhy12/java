ff8b654ffe7f03ded36b169bdc233d5273c02f71
lh031527 <lihao031527@outlook.com>
2021-01-05 20:39:44 +0800

    lhxxx

---
+++ src/main/java/com/example/javabbs/dao/LoginDao.java
@@ -0,0 +1,8 @@
+package com.example.javabbs.dao;
+
+
+import com.example.javabbs.domain.Login;
+
+public interface LoginDao {
+    int loginUser(Login login);
+}
--- src/main/java/com/example/javabbs/util/BaseDao.java
+++ src/main/java/com/example/javabbs/util/BaseDao.java
@@ -1,4 +1,54 @@
 package com.example.javabbs.util;
 
+import java.sql.*;
+
 public class BaseDao {
+    private static Connection connection;
+    private static PreparedStatement preparedStatement;
+
+    {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/bbs", "Lh12345678@", "Lh12345678@");
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Connection getConnection(){
+        if(connection == null) {
+            try {
+                connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/bbs", "Lh12345678@", "Lh12345678@");
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+        }
+        return connection;
+    }
+
+    public ResultSet executeQuery(String sql, Object... params) {
+        connection = getConnection();
+        try {
+            preparedStatement = connection.prepareStatement(sql);
+            for(int i = 0; i < params.length; i++)
+                preparedStatement.setObject(i + 1,  params[i]);
+            return preparedStatement.executeQuery();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return null;
+    }
+
+    public int executeUpdate(String sql, Object... params) {
+        connection = getConnection();
+        try {
+            preparedStatement = connection.prepareStatement(sql);
+            for(int i = 0; i < params.length; i++)
+                preparedStatement.setObject(i + 1,  params[i]);
+            return preparedStatement.executeUpdate();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return -1;
+    }
 }
